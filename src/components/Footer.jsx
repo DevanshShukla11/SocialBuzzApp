ce7e9e441b812b3ebdf998d371705f550a4a43c7
Devansh Shukla <devanshviratshukla11@gmail.com>
2022-02-06 22:13:30 +0530

    upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,13 @@ export const Footer = () => {
       <div className="card-wrapper">
         <Row className="main-footer">
           <Col xs={{ span: 24, push: 0 }} lg={{ span: 6, push: 0 }}>
-            <Card className="card-one" title="About Us" bordered={true}>
+            <Card key={1} className="card-one" title="About Us" bordered={true}>
               <p>sqwbd qwjkbdjqw fdcj few frejkg vjreg</p>
             </Card>
           </Col>
           <Col xs={{ span: 24, push: 0 }} lg={{ span: 6, push: 0 }}>
             <Card
+              key={2}
               className="card-one"
               className="card-one"
               title="Features"
@@ -22,7 +23,7 @@ export const Footer = () => {
             </Card>
           </Col>
           <Col xs={{ span: 24, push: 0 }} lg={{ span: 6, push: 0 }}>
-            <Card className="card-one" title="Release" bordered={true}>
+            <Card key={3} className="card-one" title="Release" bordered={true}>
               <p>sqwbd qwjkbdjqw fdcj few frejkg vjreg</p>
             </Card>
           </Col>
--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Row, Col } from "antd";
+import { Row, Col } from "antd";
 
 const MainFooter = () => {
   return (
